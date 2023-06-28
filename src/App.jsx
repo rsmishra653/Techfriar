84055252704c981e6433a0c883848a2ee6e4723d
Ram <ramshankar.mishra09@gmail.com>
2023-06-28 15:41:18 +0530

    Split files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './pages/Header'
 import Navbar from './pages/Navbar'
 import Main from './pages/Main'
-import './style.scss'
+import './styles/styles.scss'
 
 function App() {
     return (
